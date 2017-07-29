34f26f6f07f6bc717ee5e83f1f5c4e6c0675fcdc
JackAlexandr <ee16b007@smail.iitm.ac.in>
2017-07-29 16:38:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/draagamadhuri/viewpager2/Fragment2.java
@@ -0,0 +1,21 @@
+package com.example.draagamadhuri.viewpager2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by D.RAAGA MADHURI on 29-Jul-17.
+ */
+
+class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag2,container,false);
+    }
+}
